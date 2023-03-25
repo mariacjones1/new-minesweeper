23986e8332b0ebda594d70f77384f796621495d0
Maria <mariacjones1@hotmail.co.uk>
2023-03-25 10:39:21 +0000

    Set difficulty levels

--- src/main/java/com/softwareintitute/davis/gareth/minesweeper/Main.java
+++ src/main/java/com/softwareintitute/davis/gareth/minesweeper/Main.java
@@ -4,20 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        MinesweeperGrid myGrid = new MinesweeperGrid(2,2,1);
         Scanner scanner = new Scanner(System.in);
+        int length = 10;
+        int width = 10;
+        int numberOfMines = 10;
+        System.out.println("Choose your difficulty: Beginner (1), Intermediate (2) or Expert (3)");
+        int difficulty = scanner.nextInt();
+        if (difficulty == 1){
+            length = 10;
+            width = 10;
+            numberOfMines = 10;
+        } else if (difficulty == 2){
+            length = 16;
+            width = 16;
+            numberOfMines = 40;
+        } else if (difficulty == 3){
+            length = 16;
+            width = 30;
+            numberOfMines = 99;
+        }
+        MinesweeperGrid myGrid = new MinesweeperGrid(length,width,numberOfMines);
         int xCord;
         int yCord;
         String selection;
         System.out.println(myGrid);
         while (myGrid.runGame) {
+            scanner.nextLine();
             System.out.println("Do you want to reveal the square (R) or set a flag (F)?");
             selection = scanner.nextLine().toUpperCase();
             System.out.println("Please enter X coordinate:");
             xCord = scanner.nextInt();
             System.out.println("Please enter Y coordinate:");
             yCord = scanner.nextInt();
-            scanner.nextLine();
             if (selection.equals("R")) {
                 System.out.println(myGrid.revealSquare(xCord,yCord));
             } else if (selection.equals("F")) {
